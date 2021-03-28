17ad9ba8ee991d60254f9f0fd6a7aea0a4f950fd
HOUNKANRIN AYINLA Mouhayad I <40716257+Lino004@users.noreply.github.com>
2021-03-28 12:18:45 +0000

    Installation of start screens and Paystack + Beds24✅

--- front/src/views/Register.vue
+++ front/src/views/Register.vue
@@ -1,13 +1,50 @@
 <template>
-  <div>
-    Register
-    <b-button @click="$router.push({ name: 'PaystackBeds24' })">Go to PaystackBeds24</b-button>
+  <div class="container is-max-desktop" style="height: 100%">
+    <div class="columns is-vcentered is-mobile" style="height: 100%">
+      <div class="column">
+        <h1 class="has-text-centered title mb-6">Get started</h1>
+        <b-field 
+          label="Email Address"
+          custom-class="has-text-centered">
+          <b-input 
+            custom-class="custum-input"
+            v-model="email">
+          </b-input>
+        </b-field>
+        <div class="has-text-centered mb-5">
+          <b-checkbox 
+            v-model="privacyPolicy">
+            I agree to Terms and privacy policy
+          </b-checkbox>
+        </div>
+        <div class="columns is-centered">
+          <div class="column is-4">
+            <b-button 
+              type="is-primary"
+              expanded
+              @click="proceed">
+              Proceed
+            </b-button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 
 export default {
-  components: {}
+  data() {
+    return {
+      email: '',
+      privacyPolicy: true,
+    }
+  },
+  methods: {
+    proceed() {
+      this.$router.push({ name: 'PaystackBeds24' });
+    }
+  }
 }
 </script>
