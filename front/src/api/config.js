49adeccbfc003b1304dad659d90a01e3cf871439
HOUNKANRIN AYINLA Mouhayad I <40716257+Lino004@users.noreply.github.com>
2021-03-29 18:12:36 +0000

    correction

--- front/src/api/config.js
+++ front/src/api/config.js
@@ -2,16 +2,16 @@ let config;
 if (process.env.NODE_ENV === 'production') {
   if (process.env.VUE_APP_BACK === 'staging') {
     config = {
-      BASE_URL: 'http://localhost:4242/',
+      BASE_URL: 'https://backpaystack.herokuapp.com/',
     };
   } else {
     config = {
-      BASE_URL: 'http://localhost:4242/',
+      BASE_URL: 'https://backpaystack.herokuapp.com/',
     };
   }
 } else {
   config = {
-    BASE_URL: 'https://backlino004.herokuapp.com/',
+    BASE_URL: 'http://localhost:4242/',
   };
 }
 // config = configStaging;
