a95a8101862b87c0329bab37c332708734f989d3
HOUNKANRIN AYINLA Mouhayad I <40716257+Lino004@users.noreply.github.com>
2021-03-29 10:26:39 +0000

    Correction: retrieving the original url from the request✅

--- back/server.js
+++ back/server.js
@@ -9,6 +9,7 @@ app.use(cors());
 
 app.post('/create-checkout-session', async (req, res) => {
   const { priceId, email } = req.body;
+  const origin = req.get('origin');
   try {
     const session = await stripe.checkout.sessions.create({
       mode: "subscription",
@@ -20,8 +21,8 @@ app.post('/create-checkout-session', async (req, res) => {
           quantity: 1,
         },
       ],
-      success_url: `${config.FRONT_DOMAIN}/PaystackBeds24?type=success&session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `${config.FRONT_DOMAIN}/PaystackBeds24?type=cancel&session_id={CHECKOUT_SESSION_ID}`,
+      success_url: `${origin}/PaystackBeds24?type=success&session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${origin}/PaystackBeds24?type=cancel&session_id={CHECKOUT_SESSION_ID}`,
     });
 
     res.send({ sessionId: session.id, publishableKey: config.STRIPE_PUBLISHABLE_KEY });
