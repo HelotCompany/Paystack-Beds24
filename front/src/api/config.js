d9ff4682e151910b650e769f17fac60f828e0204
HOUNKANRIN AYINLA Mouhayad I <40716257+Lino004@users.noreply.github.com>
2021-04-03 14:31:20 +0000

    api change

--- front/src/api/config.js
+++ front/src/api/config.js
@@ -2,16 +2,16 @@ let config;
 if (process.env.NODE_ENV === 'production') {
   if (process.env.VUE_APP_BACK === 'staging') {
     config = {
-      BASE_URL: 'https://backpaystack.herokuapp.com/',
+      BASE_URL: 'https://us-central1-paystack-for-beds24.cloudfunctions.net/app/',
     };
   } else {
     config = {
-      BASE_URL: 'https://backpaystack.herokuapp.com/',
+      BASE_URL: 'https://us-central1-paystack-for-beds24.cloudfunctions.net/app/',
     };
   }
 } else {
   config = {
-    BASE_URL: 'https://backpaystack.herokuapp.com/',
+    BASE_URL: 'http://localhost:5001/paystack-for-beds24/us-central1/app/',
   };
 }
 export default config;
